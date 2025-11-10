69e5e7b3dbc304a8caad7fbefb8547ea4e7be03e
Gaston <gichuregaston@gmail.com>
2025-11-10 15:41:40 +0300

    made config file, config manager & json converter

--- src/main/java/com/gaston/httpserver/HttpServer.java
+++ src/main/java/com/gaston/httpserver/HttpServer.java
@@ -1,4 +1,4 @@
-package com.gaston.htttpserver;
+package com.gaston.httpserver;
 // Driver class for my htttp server
 public class HttpServer {
     public static void main(String[] args) {
--- src/main/java/com/gaston/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/gaston/httpserver/config/ConfigurationManager.java
@@ -1,4 +1,15 @@
 package com.gaston.httpserver.config;
 
 public class ConfigurationManager {
+ private static ConfigurationManager myConfigurationManager;
+ private static Configuration myCurrentConfiguration;
+ private ConfigurationManager (){};
+public static ConfigurationManager getInstance(){
+    if(myConfigurationManager == null)
+        myConfigurationManager = new ConfigurationManager();
+    return myConfigurationManager;
 }
+public void loadConfigurationFile(String filePath){}
+    public void getCurrentConfiguration(){ }
+}
+
