7444f997740752b04dc0f000d2b5c19414806025
Gaston <gichuregaston@gmail.com>
2025-11-11 12:33:35 +0300

    Fixed parsing error for file configuration

--- src/main/java/com/gaston/httpserver/HttpServer.java
+++ src/main/java/com/gaston/httpserver/HttpServer.java
@@ -1,8 +1,19 @@
 package com.gaston.httpserver;
-// Driver class for my htttp server
+
+import com.gaston.httpserver.config.Configuration;
+import com.gaston.httpserver.config.ConfigurationManager;
+
+import java.util.Arrays;
+
+// Driver class for my http server
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("Server Start!");
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("Using Port : "+conf.getPort());
+        System.out.println("Using WebRoot : "+conf.getWebroot());
+
 
     }
 }
